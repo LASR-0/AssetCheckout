b4eb9e14da3df61f4adf6072940c69e07d2ace3c
WhatTheShuck <Contact@brandonwiedman.com>
2026-05-26 13:04:30 +1000

    Fix API being hardcoded in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-  proxy: {
-    '/api': {
-      target: 'http://127.0.0.1:3000',
-      changeOrigin: true,
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+        },
+      },
     },
-  },
-}
-})
+  };
+});
